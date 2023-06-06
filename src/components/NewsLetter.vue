e7810064a740b0f8d47cbb98c8745db970a832db
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-06 17:11:23 +0100

    restructure form

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -7,13 +7,13 @@
         <img class="object-cover object-center rounded" alt="hero" src="../assets/contact-image.png">
       </div>
       <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+        <h2 class="text-gray-900 text-2xl mb-1 font-bold title-font">Send us a Message now</h2>
+        <p class="leading-relaxed mb-5 text-gray-600 text-md">You are only a few steps away from obtaining the
+          information
+          you
+          require. However, if you send the message now, our team will contact you right away</p>
+          
         <form name="contact" netlify method="POST" data-netlify="true">
-          <input type="hidden" name="form-name" value="contact" />
-          <h2 class="text-gray-900 text-2xl mb-1 font-bold title-font">Send us a Message now</h2>
-          <p class="leading-relaxed mb-5 text-gray-600 text-md">You are only a few steps away from obtaining the
-            information
-            you
-            require. However, if you send the message now, our team will contact you right away</p>
           <div class="relative mb-4">
             <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
             <input type="text" id="name" name="name"
@@ -36,7 +36,8 @@
             <input type="checkbox" name="" id="" class=" px-6" required>
             <p class="text-lg text-gray-500 px-2 ">You agree to the terms and condition.</p>
           </div>
-          <button class="text-white border-0 py-2 px-6 focus:outline-none rounded text-lg mt-6 btn-sign">Contact Us</button>
+          <button class="text-white border-0 py-2 px-6 focus:outline-none rounded text-lg mt-6 btn-sign">Contact
+            Us</button>
 
         </form>
       </div>

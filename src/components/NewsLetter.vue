f5e9637c1cdf4d37c2f20d38c92c45c41f31fa63
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-06 17:16:21 +0100

    fix form structure

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -12,34 +12,29 @@
           information
           you
           require. However, if you send the message now, our team will contact you right away</p>
-          
-        <form name="contact" netlify method="POST" data-netlify="true">
-          <div class="relative mb-4">
-            <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name"
-              class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 "
-              required>
-          </div>
-          <div class="relative mb-4">
-            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" placeholder="email@example.com"
-              class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8"
-              required>
-          </div>
-          <div class="relative mb-4">
-            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-            <textarea id="message" type="textarea" name="message"
-              class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              required></textarea>
-          </div>
-          <div class="flex items-center mt-4 ">
-            <input type="checkbox" name="" id="" class=" px-6" required>
-            <p class="text-lg text-gray-500 px-2 ">You agree to the terms and condition.</p>
-          </div>
-          <button class="text-white border-0 py-2 px-6 focus:outline-none rounded text-lg mt-6 btn-sign">Contact
-            Us</button>
 
-        </form>
+       	<form name="contact" netlify method="POST" data-netlify="true">
+					<div class="relative mb-4">
+						<label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+						<input type="text" id="name" name="name"
+							class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+					</div>
+					<div class="relative mb-4">
+						<label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+						<input type="email" id="email" name="email"
+							class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+					</div>
+					<div class="relative mb-4">
+						<label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+						<textarea id="message" name="message"
+							class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+					</div>
+					<button
+						class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+					<p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook
+						viral
+						artisan.</p>
+				</form>
       </div>
 
     </div>

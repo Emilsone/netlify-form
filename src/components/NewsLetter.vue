8bb54610df0bfc3b4d68c44f9376b3b1b6fba039
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-06 13:59:05 +0100

    add input

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
         <form name="contact" netlify method="POST" data-netlify="true">
-          <!-- <input type="hidden" name="form-name" value="contact" /> -->
+          <input type="hidden" name="form-name" value="contact" />
           <h2 class="text-gray-900 text-2xl mb-1 font-bold title-font">Send us a Message now</h2>
           <p class="leading-relaxed mb-5 text-gray-600 text-md">You are only a few steps away from obtaining the
             information
@@ -36,7 +36,7 @@
             <input type="checkbox" name="" id="" class=" px-6" required>
             <p class="text-lg text-gray-500 px-2 ">You agree to the terms and condition.</p>
           </div>
-          <button class="text-white border-0 py-2 px-6 focus:outline-none  rounded text-lg mt-6 btn-sign">Contact Us</button>
+          <button class="text-white border-0 py-2 px-6 focus:outline-none rounded text-lg mt-6 btn-sign">Contact Us</button>
 
         </form>
       </div>
